24744491ac679096515b0ed97cf818324cb26d4d
huynguyenn0103 <113884736+huynguyenn0103@users.noreply.github.com>
2022-12-14 21:21:58 +0700

    update array exercise

--- src/Loopexercise.java
+++ src/Loopexercise.java
@@ -8,7 +8,6 @@ public class Loopexercise {
             count++;
         }
     }
-
     public static void Bai2() {
         Scanner scanner = new Scanner(System.in);
         double sum = 0;
@@ -20,7 +19,6 @@ public class Loopexercise {
         }
         System.out.println("Tong cac so vua nhap la: " + sum);
     }
-
     public static void Bai3() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("-----------MENU-----------");
@@ -100,7 +98,193 @@ public class Loopexercise {
         }
         System.out.println("Tong cac so chan nho hon n la: " + sum);
     }
+    public static void Bai7(){
+        int count = 0;
+        for(int i = 0; i <= 1000; i++){
+            if(i % 3 == 0) count++;
+        }
+        System.out.println("Co tat ca " + count + " so chia het cho 3 tu 0 den 1000");
+    }
+    public static void Bai8(){
+        Scanner scanner = new Scanner(System.in);
+        String[] arrMachine = {"keo", "bua", "bao"};
+        int scoreMachine = 0;
+        int scoreHuman = 0;
+        while(true){
+            int random = (int) (Math.random() + 1);
+            System.out.print("Nhap lua chon cua ban la: ");
+            String choice = scanner.next();
+            System.out.println("Lua chon cua ban la: " + choice);
+            System.out.println("Lua chon cua may la: " + arrMachine[random]);
+            if(solveWinner(choice, arrMachine[random]) == -1) {
+                System.out.println("Ket qua: May thang");
+                System.out.println("Ti so giua nguoi va may: " + scoreHuman + " " + (++scoreMachine));
+            }
+            else if(solveWinner(choice, arrMachine[random]) == 0){
+                System.out.println("Ket qua: Hoa");
+                System.out.println("Ti so giua nguoi va may: " + scoreHuman + " " + scoreMachine);
+            }
+            else {
+                System.out.println("Ket qua: Nguoi thang");
+                System.out.println("Ti so giua nguoi va may: " + (++scoreHuman) + " " + scoreMachine);
+            }
+            System.out.print("Nhan mot so khac 0 de choi tiep: ");
+            int selection = scanner.nextInt();
+            if(selection == 0) {
+                if(scoreMachine > scoreHuman) System.out.println("May thang nhieu hon");
+                else if(scoreMachine < scoreHuman) System.out.println("Nguoi thang nhieu hon");
+                else System.out.println("Hoa");
+                return;
+            }
+            System.out.println();
+        }
 
+    }
+    public static void Bai9(){
+        int sum = 0;
+        int count = 1;
+        while(sum <= 10000){
+            sum += count;
+            count++;
+        }
+        System.out.println("So n la: " + count);
+    }
+    public static void Bai10(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so nguyen n: ");
+        int n = scanner.nextInt();
+        while(n <= 0){
+            System.out.print("Nhap lai gia tri n > 0: ");
+             n = scanner.nextInt();
+        }
+        int sum = 0;
+        for(int i = 1; i < n; i++){
+            if(i % 2 != 0) sum = sum + i;
+        }
+        System.out.println("Tong cac so le nguyen duong nho hon n: " + sum);
+    }
+    public static void Bai11(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap gia tri n: ");
+        double n = scanner.nextDouble();
+        System.out.print("Nhap gia tri x: ");
+        double x = scanner.nextDouble();
+        double sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum = sum + Math.pow(x,i);
+        }
+        System.out.println("Tong can tim la: " + sum);
+    }
+    public static void Bai12(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so tien anh X gui: ");
+        long sotien = scanner.nextLong();
+        System.out.print("Nhap so tien anh X muon co trong tuong lai: ");
+        long sotientuonglai = scanner.nextLong();
+        System.out.print("Nhap lai suat %: ");
+        double laisuat = scanner.nextDouble();
+        int soNam = 0;
+        while(sotien * Math.pow(1 + laisuat / 100,soNam)< sotientuonglai){
+            soNam++;
+        }
+        System.out.println("So nam it nhat can gui: " + soNam);
+    }
+    public static void Bai13(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so nguyen n: ");
+        int n = scanner.nextInt();
+        int [] arr = new int [n];
+        // Ham nhap
+        for(int i = 0; i<  n; i++){
+            System.out.print("Nhap 1 phan tu: ");
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println();
+        //Ham xuat
+        System.out.print("Cac phan tu cua mang la:  ");
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        // Tim so lon nhat va vi tri cua no trong mang
+        int max = -1000000;
+        int index = 0;
+        for(int i = 0;i < n; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                index = i;
+            }
+        }
+        System.out.println("So lon nhat la : " + max + " tai vi tri " + index);
+
+        // Tim so am dau tien va vi tri cua no trong mang
+        int soam = 1;
+        int indexsoam = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] < 0){
+                soam = arr[i];
+                indexsoam = i;
+                break;
+            }
+        }
+        if(soam == 1) System.out.println("Khong co so am trong day");
+        else System.out.println("So am dau tien la: " + soam + " tai vi tri " + indexsoam);
+
+        // Tim so am lon nhat va vi tri cua no trong mang
+        int soamlonnhat = -10000000;
+        int indexsoamlonnhat = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] < 0 && arr[i] > soamlonnhat){
+                soamlonnhat = arr[i];
+                indexsoamlonnhat = i;
+            }
+        }
+        if(soamlonnhat == -10000000) System.out.println("Khong co so am trong day");
+        else System.out.println("So am lon nhat la: " + soamlonnhat + " tai vi tri " + indexsoamlonnhat);
+
+        // Tinh tong cac so chan
+        int sumEven =0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] % 2 == 0) sumEven = sumEven + arr[i];
+        }
+        System.out.println("Tong cac so chan trong mang: " + sumEven);
+
+        // Dem co bao nhieu so am
+        int countNegative = 0;
+        for(int i =0; i < n; i++){
+            if(arr[i] < 0) countNegative++;
+        }
+        System.out.println("So so am trong mang: " + countNegative);
+
+        //Tinh tong cac so am
+        int sumNe = 0;
+        for(int i = 0; i< n; i++){
+            if(arr[i] < 0) sumNe += arr[i];
+        }
+        System.out.println("Tong cac so am trong mang: " + sumNe);
+
+        // Tim x co trong mang hay khong
+        System.out.print("Nhap so nguyen x can tim: ");
+        int x = scanner.nextInt();
+        boolean find = false;
+        for(int i =0; i <n; i++){
+            if(arr[i] == x){
+                find = true;
+                System.out.println("=> Tim thay gia tri x trong mang");
+                break;
+            }
+        }
+        if(!find) System.out.println("=>Khong tim thay gia tri x trong mang");
+    }
+    public static int solveWinner(String human, String machine) {
+        if ((human.compareTo("bua") == 0  && machine.compareTo("keo") == 0 ) || (human.compareTo("keo") == 0 && machine.compareTo("bao")==0) || (human.compareTo("bao")== 0 && machine.compareTo("bua")==0)) {
+            return 1; // human thang
+        }
+        else if ((machine.compareTo("bua") == 0  && human.compareTo("keo") == 0 ) || (machine.compareTo("keo") == 0 && human.compareTo("bao")==0) || (machine.compareTo("bao")== 0 && human.compareTo("bua")==0)) {
+            return -1; // human thua
+        }
+        else return 0; // hoa
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Bai muon thuc hien: ");
@@ -128,6 +312,34 @@ public class Loopexercise {
             }
             case 6:{
                 Bai6();
+                break;
+            }
+            case 7: {
+                Bai7();
+                break;
+            }
+            case 8: {
+                Bai8();
+                break;
+            }
+            case 9: {
+                Bai9();
+                break;
+            }
+            case 10:{
+                Bai10();
+                break;
+            }
+            case 11:{
+                Bai11();
+                break;
+            }
+            case 12: {
+                Bai12();
+                break;
+            }
+            case 13:{
+                Bai13();
                 break;
             }
             default:
